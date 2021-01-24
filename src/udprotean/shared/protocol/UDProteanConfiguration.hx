62ae4ede60d78ab4dbcdf924bfe9584324dc5c11
dpomier <dimitri.pomier@gmail.com>
2021-01-24 15:14:26 +0100

    [macro] disable config from defines

--- src/udprotean/shared/protocol/UDProteanConfiguration.hx
+++ src/udprotean/shared/protocol/UDProteanConfiguration.hx
@@ -6,7 +6,6 @@ import haxe.macro.Context;
 
 class UDProteanConfiguration
 {
-    #if !macro
     /**
      * The size of the sequential communication cyclic buffers.
      * Increasing this number makes the protocol more resistant to
@@ -15,7 +14,7 @@ class UDProteanConfiguration
      *
      * **This option needs to have the same value on both peers.**
      */
-    public static inline var SequenceSize: Int                 = getOrDefault("UDPROTEAN_SEQUENCE_SIZE", 512);
+    public static inline var SequenceSize: Int                 = #if !macro getOrDefault("UDPROTEAN_SEQUENCE_SIZE", 512) #else 512 #end;
 
 
     /**
@@ -27,21 +26,21 @@ class UDProteanConfiguration
      *
      * **This option needs to have the same value on both peers.**
      */
-    public static inline var FragmentSize: Int                 = getOrDefault("UDPROTEAN_FRAGMENT_SIZE", 540);
+    public static inline var FragmentSize: Int                 = #if !macro getOrDefault("UDPROTEAN_FRAGMENT_SIZE", 540) #else 540 #end;
 
 
     /**
      * The time (in ms) after which a not acknowledged datagram should be re-sent.
      * A larger value is recommended for servers with multiple clients.
      */
-    public static inline var RepeatDatagramAge: Float            = getOrDefault("UDPROTEAN_REPEAT_AGE", 50);
+    public static inline var RepeatDatagramAge: Float            = #if !macro getOrDefault("UDPROTEAN_REPEAT_AGE", 50) #else 50 #end;
 
 
     /**
      * The time (in ms) after which retransmission of a datagram should be requested when receiving one out-of-order.
      * A larger value is recommended for servers with multiple clients.
      */
-    public static inline var StaleDatagramAge: Float             = getOrDefault("UDPROTEAN_STALE_AGE", 20);
+    public static inline var StaleDatagramAge: Float             = #if !macro getOrDefault("UDPROTEAN_STALE_AGE", 20) #else 20 #end;
 
 
     /**
@@ -51,7 +50,7 @@ class UDProteanConfiguration
      *
      * **This option needs to have the same value on both peers.**
      */
-    public static inline var SequenceBytes: Int                = getOrDefault("UDPROTEAN_SEQUENCE_BYTES", 3);
+    public static inline var SequenceBytes: Int                = #if !macro getOrDefault("UDPROTEAN_SEQUENCE_BYTES", 3) #else 3 #end;
 
 
     /**
@@ -62,21 +61,19 @@ class UDProteanConfiguration
      * legitimate datagrams to be discarded during heavy network traffic and/or significant packet loss, when the
      * head of the cyclic buffer approaches its tail.
      */
-    public static inline var SequenceDistanceRelationship: Int = getOrDefault("UDPROTEAN_SEQUENCE_DISTANCE", 32);
+    public static inline var SequenceDistanceRelationship: Int = #if !macro getOrDefault("UDPROTEAN_SEQUENCE_DISTANCE", 32) #else 32 #end;
 
 
     /**
      * The time (in seconds) of inactivity after which the client will send a keep-alive ping message to the server.
      */
-    public static inline var ClientPingInterval: Float           = getOrDefault("UDPROTEAN_PING_INTERVAL", 1.0);
+    public static inline var ClientPingInterval: Float           = #if !macro getOrDefault("UDPROTEAN_PING_INTERVAL", 1.0) #else 1.0 #end;
 
 
     /**
      * The time (in seconds) of inactivity after which a client is considered disconnected and removed from the server.
      */
-    public static inline var ClientIdleTimeLimit: Float          = getOrDefault("UDPROTEAN_CLIENT_IDLE_TIME", 3.0);
-
-    #end
+    public static inline var ClientIdleTimeLimit: Float          = #if !macro getOrDefault("UDPROTEAN_CLIENT_IDLE_TIME", 3.0) #else 3.0 #end;
 
 
     @IgnoreCover
